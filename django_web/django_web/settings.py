a9595a893a9dc00142584b5c1c20b3a9406e1687
gaga.yan <yianjiajia@126.com>
2014-12-05 18:39:32 +0800

    12/5

--- django_web/django_web/settings.py
+++ django_web/django_web/settings.py
@@ -33,6 +33,7 @@ TEMPLATE_DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
